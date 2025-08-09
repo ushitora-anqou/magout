cca45925afee194f3447f21d5ed08378786c7e3c
Ushitora Anqou <ushitora@anqou.net>
2025-08-09 11:44:20 +0900

    set timeout to kubernetes client

--- cmd/main.go
+++ cmd/main.go
@@ -181,7 +181,9 @@ func mainController() error {
 		metricsServerOptions.FilterProvider = filters.WithAuthenticationAndAuthorization
 	}
 
-	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+	cfg := ctrl.GetConfigOrDie()
+	cfg.Timeout = 1 * time.Minute
+	mgr, err := ctrl.NewManager(cfg, ctrl.Options{
 		Scheme:                 scheme,
 		Metrics:                metricsServerOptions,
 		WebhookServer:          webhookServer,
